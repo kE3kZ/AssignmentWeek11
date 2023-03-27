0dd3e093c89da70fdb654ac7e17c1a946fbc41fa
kE3kZ <keekli@gmail.com>
2023-03-26 22:11:05 -0500

    first commit

---
+++ Controllers/CulturesController.cs
@@ -0,0 +1,163 @@
+﻿using AssignmentWeek11.Data;
+using AssignmentWeek11.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentWeek11.Controllers
+{
+    public class CulturesController : Controller
+    {
+        private readonly AdventureWorks2019Context _context;
+
+        public CulturesController(AdventureWorks2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Cultures
+        public async Task<IActionResult> Index()
+        {
+            return _context.Culture != null ?
+                        View(await _context.Culture.ToListAsync()) :
+                        Problem("Entity set 'AdventureWorks2019Context.Culture'  is null.");
+        }
+
+        // GET: Cultures/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Culture == null)
+            {
+                return NotFound();
+            }
+
+            var culture = await _context.Culture
+                .FirstOrDefaultAsync(m => m.CultureId == id);
+            if (culture == null)
+            {
+                return NotFound();
+            }
+
+            return View(culture);
+        }
+
+        // GET: Cultures/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cultures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CultureId,Name,ModifiedDate")] Culture culture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(culture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(culture);
+        }
+
+        // GET: Cultures/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Culture == null)
+            {
+                return NotFound();
+            }
+
+            var culture = await _context.Culture.FindAsync(id);
+            if (culture == null)
+            {
+                return NotFound();
+            }
+            return View(culture);
+        }
+
+        // POST: Cultures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CultureId,Name,ModifiedDate")] Culture culture)
+        {
+            if (id != culture.CultureId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(culture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CultureExists(culture.CultureId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(culture);
+        }
+
+        // GET: Cultures/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Culture == null)
+            {
+                return NotFound();
+            }
+
+            var culture = await _context.Culture
+                .FirstOrDefaultAsync(m => m.CultureId == id);
+            if (culture == null)
+            {
+                return NotFound();
+            }
+
+            return View(culture);
+        }
+
+        // POST: Cultures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Culture == null)
+            {
+                return Problem("Entity set 'AdventureWorks2019Context.Culture'  is null.");
+            }
+            var culture = await _context.Culture.FindAsync(id);
+            if (culture != null)
+            {
+                _context.Culture.Remove(culture);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CultureExists(string id)
+        {
+            return (_context.Culture?.Any(e => e.CultureId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,186 @@
+﻿using AssignmentWeek11.Data;
+using AssignmentWeek11.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentWeek11.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AdventureWorks2019Context _context;
+
+        public ProductsController(AdventureWorks2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var adventureWorks2019Context = _context.Product.Include(p => p.ProductModel).Include(p => p.ProductSubcategory).Include(p => p.SizeUnitMeasureCodeNavigation).Include(p => p.WeightUnitMeasureCodeNavigation);
+            return View(await adventureWorks2019Context.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.ProductModel)
+                .Include(p => p.ProductSubcategory)
+                .Include(p => p.SizeUnitMeasureCodeNavigation)
+                .Include(p => p.WeightUnitMeasureCodeNavigation)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModel, "ProductModelId", "Name");
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategory, "ProductSubcategoryId", "Name");
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode");
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,Size,SizeUnitMeasureCode,WeightUnitMeasureCode,Weight,DaysToManufacture,ProductLine,Class,Style,ProductSubcategoryId,ProductModelId,SellStartDate,SellEndDate,DiscontinuedDate,Rowguid,ModifiedDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModel, "ProductModelId", "Name", product.ProductModelId);
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategory, "ProductSubcategoryId", "Name", product.ProductSubcategoryId);
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode", product.SizeUnitMeasureCode);
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode", product.WeightUnitMeasureCode);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModel, "ProductModelId", "Name", product.ProductModelId);
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategory, "ProductSubcategoryId", "Name", product.ProductSubcategoryId);
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode", product.SizeUnitMeasureCode);
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode", product.WeightUnitMeasureCode);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,Size,SizeUnitMeasureCode,WeightUnitMeasureCode,Weight,DaysToManufacture,ProductLine,Class,Style,ProductSubcategoryId,ProductModelId,SellStartDate,SellEndDate,DiscontinuedDate,Rowguid,ModifiedDate")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModel, "ProductModelId", "Name", product.ProductModelId);
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategory, "ProductSubcategoryId", "Name", product.ProductSubcategoryId);
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode", product.SizeUnitMeasureCode);
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasure, "UnitMeasureCode", "UnitMeasureCode", product.WeightUnitMeasureCode);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.ProductModel)
+                .Include(p => p.ProductSubcategory)
+                .Include(p => p.SizeUnitMeasureCodeNavigation)
+                .Include(p => p.WeightUnitMeasureCodeNavigation)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Product == null)
+            {
+                return Problem("Entity set 'AdventureWorks2019Context.Product'  is null.");
+            }
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return (_context.Product?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+    }
+}
